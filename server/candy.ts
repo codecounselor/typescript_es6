014a7b5bb8f638eddbbc544dc8fcd0c1db13bdfc
Nate Good <codecounselor@gmail.com>
2016-03-28 17:12:02 -0400

    added nodemon dependency and changed test from ts to js to not get typescript compiler errors

--- server/candy.ts
+++ server/candy.ts
@@ -2,6 +2,11 @@ export default class Candy {
 
   private weight: number;
 
+  /**
+   *
+   * @param type The kind of candy
+   * @param color The color of the candy
+     */
   constructor(public type: string, public color: string) { }
 
   public addWeight(weight) {
--- server/server.ts
+++ server/server.ts
@@ -4,6 +4,7 @@ import * as express from 'express';
 
 const app = express();
 
+app.get('/', (req, resp) => resp.send('Hello Java(Script) Enthusiasts'));
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
